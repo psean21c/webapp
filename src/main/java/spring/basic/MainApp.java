0b6f192eef7bcc68528ba1d155f86edec63393f5
unknown <psean21c@gmail.com>
2017-12-07 15:55:36 -0500

    updated Spring

--- src/main/java/spring/basic/MainApp.java
+++ src/main/java/spring/basic/MainApp.java
@@ -3,10 +3,22 @@ package spring.basic;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.basic.inject.TextEditor;
+
 public class MainApp {
-   public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-      obj.getMessage();
-   }
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		// 1) hello
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+
+		// 2) Inject
+		TextEditor te = (TextEditor) context.getBean("textEditor");
+		te.spellCheck();
+
+		String ctxID = context.getId();
+		System.out.println(ctxID + "");
+	}
 }
