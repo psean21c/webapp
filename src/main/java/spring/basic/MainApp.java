47d7b4037098ddf307a1a82bd478ae07241cb1fd
unknown <psean21c@gmail.com>
2017-12-08 12:05:06 -0500

    updated spring

--- src/main/java/spring/basic/MainApp.java
+++ src/main/java/spring/basic/MainApp.java
@@ -1,35 +1,47 @@
 package spring.basic;
 
+import org.springframework.core.SpringVersion;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.basic.inject.lab2.Lecture;
 
-
 public class MainApp {
 
-	 @SuppressWarnings("resource")
 	public static void main(String[] args) {
+
+		// runAutoWired();
+		runSpringVersion();
+	}
+
+	static void runSpringVersion() {
+		// org.springframework.core.SpringVersion.getVersion()
+		System.out.println(SpringVersion.getVersion());
+
+	}
+
+	@SuppressWarnings("resource")
+	static void runAutoWired() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-//		// 1) hello
-//		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-//		obj.getMessage();
-//
-//		// 2) Inject
-//		TextEditor te = (TextEditor) context.getBean("textEditor");
-//		te.spellCheck();
-//
-//		// 3) Required
-//		Student student = (Student) context.getBean("pupil");
-//		System.out.println("Name : " + student.getName());
-//		System.out.println("Age : " + student.getAge());
+		// // 1) hello
+		// HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		// obj.getMessage();
+		//
+		// // 2) Inject
+		// TextEditor te = (TextEditor) context.getBean("textEditor");
+		// te.spellCheck();
+		//
+		// // 3) Required
+		// Student student = (Student) context.getBean("pupil");
+		// System.out.println("Name : " + student.getName());
+		// System.out.println("Age : " + student.getAge());
 
 		// 4) Autowired
 		Lecture lecture = (Lecture) context.getBean("lecture");
 		lecture.doMark();
-		
-//		String ctxID = context.getId();
-//		System.out.println(ctxID + "");
+
+		// String ctxID = context.getId();
+		// System.out.println(ctxID + "");
 
 	}
 }
