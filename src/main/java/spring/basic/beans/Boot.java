8ea204c6eb8b9d05782e5449e192b42404e92852
unknown <psean21c@gmail.com>
2017-12-19 14:02:54 -0500

    updated spring

--- src/main/java/spring/basic/beans/Boot.java
+++ src/main/java/spring/basic/beans/Boot.java
@@ -2,14 +2,16 @@ package spring.basic.beans;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-//import spring.basic.beans.Messenger;
+import spring.basic.beans.DemoBeanPostProcessor;
 
 public class Boot {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-//		Messenger messenger = (Messenger) ctx.getBean("messenger");
-//		System.out.println(messenger);
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("./spring/basic/beans/spring2.xml");
+		DemoBeanPostProcessor demo = (DemoBeanPostProcessor) ctx.getBean("demo");
+		System.out.println(demo);
+		demo.postProcessAfterInitialization(demo, "test1");
+		demo.postProcessBeforeInitialization(demo, "test1");
 	}
 
 }
--- src/main/java/spring/basic/beans/DemoBeanPostProcessor.java
+++ src/main/java/spring/basic/beans/DemoBeanPostProcessor.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class DemoBeanPostProcessor implements BeanPostProcessor{
 
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("Before Bean[" + beanName + "] created:" + bean.toString());
 		return bean;
 	}
 
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("Bean[" + beanName + "] created:" + bean.toString());
+		System.out.println("After Bean[" + beanName + "] created:" + bean.toString());
 		return bean;
 	}
 
