2d3a264129df7b2e3d7d424eafaa68fc87d9c010
unknown <psean21c@gmail.com>
2018-01-02 14:06:28 -0500

    updated Spring BPP

--- src/main/java/spring/basic/beans/Messenger.groovy
+++
@@ -1,4 +0,0 @@
-class Messenger {
-
-    def name
-}
--- src/main/java/spring/basic/beans/SpringApp.java
+++ src/main/java/spring/basic/beans/SpringApp.java
@@ -4,14 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.basic.beans.DemoBeanPostProcessor;
 
-public class Boot {
+public class SpringApp {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("./spring/basic/beans/spring2.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("./spring/basic/beans/springBPP.xml");
 		DemoBeanPostProcessor demo = (DemoBeanPostProcessor) ctx.getBean("demo");
 		System.out.println(demo);
-		demo.postProcessAfterInitialization(demo, "test1");
-		demo.postProcessBeforeInitialization(demo, "test1");
+		demo.postProcessAfterInitialization(demo, "test2-after");
+		demo.postProcessBeforeInitialization(demo, "test1-before");
 	}
 
 }
