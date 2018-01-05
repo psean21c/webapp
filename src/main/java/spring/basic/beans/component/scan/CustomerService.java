bfddab4a38cbef9ef05da24d036c775c5f01a690
unknown <psean21c@gmail.com>
2018-01-05 12:24:37 -0500

    updated spring

--- src/main/java/spring/basic/beans/component/scan/CustomerService.java
+++ src/main/java/spring/basic/beans/component/scan/CustomerService.java
@@ -1,10 +1,12 @@
 package spring.basic.beans.component.scan;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerService {
 
+	@Autowired
 	CustomerDAO customerDAO;
 
 	public void setCustomerDAO(CustomerDAO customerDAO) {
