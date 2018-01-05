5cf215ffa1da2b53fd67c2e8970de8298912d849
unknown <psean21c@gmail.com>
2018-01-05 12:23:39 -0500

    updated spring

--- src/main/java/spring/basic/beans/component/scan/CustomerDAO.java
+++ src/main/java/spring/basic/beans/component/scan/CustomerDAO.java
@@ -1,5 +1,8 @@
 package spring.basic.beans.component.scan;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerDAO {
 	@Override
 	public String toString() {
--- src/main/java/spring/basic/beans/component/scan/CustomerService.java
+++ src/main/java/spring/basic/beans/component/scan/CustomerService.java
@@ -1,5 +1,8 @@
 package spring.basic.beans.component.scan;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
 
 	CustomerDAO customerDAO;
