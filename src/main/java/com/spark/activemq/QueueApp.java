ad221501027e008e2844bf68466fe26c1eb18389
unknown <psean21c@gmail.com>
2018-01-29 20:49:00 -0500

    updated queue 3/7

--- src/main/java/com/spark/activemq/QueueApp.java
+++ src/main/java/com/spark/activemq/QueueApp.java
@@ -3,11 +3,7 @@ package com.spark.activemq;
 public class QueueApp {
 
 	public static void main(String[] args) {
-		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-
-		Connection conn = factory.createConnection();
-		conn.start();
-
+		
 	}
 
 }
