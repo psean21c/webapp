98401531d83dde561bceafa6ec407eda3134a23a
unknown <psean21c@gmail.com>
2018-02-03 11:00:06 -0500

    updated queue 2/7

--- src/main/java/com/spark/mq/websphere/Producer.java
+++ src/main/java/com/spark/mq/websphere/Producer.java
@@ -1,14 +1,16 @@
 package com.spark.mq.websphere;
 
-import javax.jms.Connection;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
+import javax.jms.Message;
+import javax.jms.QueueConnection;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import com.ibm.mq.jms.MQConnectionFactory;
+import com.ibm.mq.jms.JMSC;
+import com.ibm.mq.jms.MQQueue;
 import com.ibm.mq.jms.MQQueueConnectionFactory;
 
 public class Producer {
@@ -17,69 +19,80 @@ public class Producer {
 	// API file:///C:/Program%20Files%20(x86)/IBM/WebSphere%20MQ/java/doc/WMQJMSClasses/index.html
 	
 	public static void main(String[] args) {
-		try {
-			/***
-			// Create a ConnectionFactory
-			MQQueueConnectionFactory factory = new MQQueueConnectionFactory();
-			String host = "localhost";
-			String port = "1463";
-	        factory.setHostName(host);
-	        factory.setPort(Integer.parseInt(port));
-
-			// Create a connection
-			Connection conn = factory.createConnection();
-			conn.start();
-	        **/
-			//MQQueueConnectionFactory factory = new MQQueueConnectionFactory("tcp://192.168.40.144:61616");
-			MQQueueConnectionFactory factory = new MQQueueConnectionFactory();
-//			factory.setLocalAddress("localhost");
-//			factory.setPort(1463);
-//			factory.setQueueManager("TBKS2");
-//			factory.setChannel("TBKS2.ERDSFX.N2");
-
-			 MQConnectionFactory cf = new MQConnectionFactory();
-			 //CommonConstants.WMQ_CONNECTION_MODE;
-			 
-//			    assertThat(cf.getIntProperty(CommonConstants.WMQ_CONNECTION_MODE), 
-//			    		is(equalTo(CommonConstants.WMQ_CM_BINDINGS)));
-//			    cf.setIntProperty(CommonConstants.WMQ_CONNECTION_MODE, CommonConstants.WMQ_CM_CLIENT);
-//			    assertThat(cf.getIntProperty(CommonConstants.WMQ_CONNECTION_MODE),
-//			    		is(equalTo(CommonConstants.WMQ_CM_CLIENT)));
-//			}
-		
-			String host = "localhost";
-			String port = "1463";
-	        factory.setHostName(host);
-	        factory.setPort(Integer.parseInt(port));
-			factory.setQueueManager("TBKS2");
-			factory.setChannel("TBKS2.ERDSFX.N2");
-			
-			// Create a connection
-			Connection conn = factory.createConnection();
-			conn.start();
-			
-			
-			// Create a Session
-			Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-			// Create a destination (Topic / Queue)
-			Destination dest1 = session.createQueue("HELLO.Q");
-			MessageProducer producer = session.createProducer(dest1);
-			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-
-			String txt = "Hello World! " + Thread.currentThread().getName();
-			TextMessage message = session.createTextMessage(txt);
-
-			producer.send(message);
-
-			session.close();
-			conn.close();
-			
-		} catch (JMSException e) {
-			System.out.println(" JMS Error:" + e);
-		} catch (Exception e){
-			System.out.println(" Error:" + e);
-		}
+		callQueue();
 	}
+	
+    static void callQueue(){
+    	System.out.println("call MQ");
 
+    	/***
+		JMSException:javax.jms.JMSSecurityException: MQJMS2013: 
+		invalid security authentication supplied for MQQueueManager,MQJMS2013: 
+		invalid security authentication supplied for MQQueueManager
+		>> Need to use system reserved words: SYSTEM.DEF.SVRCONN
+    		***/
+		String host = "localhost";
+		String port = "1414";
+		String channel = "SYSTEM.DEF.SVRCONN";
+		String queueManager = "QM1";
+		String ccsId = "850";
+		String queueName = "Q1";
+		String userName = "WHO";    			
+    	
+  	
+        MQQueueConnectionFactory queueConnectionFactory = new MQQueueConnectionFactory();
+
+        queueConnectionFactory.setHostName(host);
+		try {
+			System.out.println("Lab-1:");
+			queueConnectionFactory.setPort(Integer.parseInt(port));
+			queueConnectionFactory.setChannel(channel);
+			queueConnectionFactory.setQueueManager(queueManager);
+			queueConnectionFactory.setCCSID(Integer.parseInt(ccsId));
+			queueConnectionFactory.setTransportType(JMSC.MQJMS_TP_CLIENT_MQ_TCPIP);
+
+			// 1) Spring
+//			UserCredentialsConnectionFactoryAdapter adapter = new UserCredentialsConnectionFactoryAdapter();
+//			adapter.setTargetConnectionFactory(queueConnectionFactory);
+//			adapter.setUsername(userName);
+//			QueueConnection queueConnection = adapter.createQueueConnection();
+
+			// 2) JAVAX JMS
+			QueueConnection queueConnection = queueConnectionFactory.createQueueConnection();
+			
+					
+			QueueSession queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			MQQueue queue = (MQQueue) queueSession.createQueue("queue:///" + queueName);
+
+			QueueSender queueSender = queueSession.createSender(queue);
+			TextMessage textMessage = queueSession.createTextMessage();
+			System.out.println("Lab-2:" + queueName + " starting");
+			queueConnection.start();
+
+			System.out.println("Lab-3:");
+			textMessage.setText("message sent-2");
+			queueSender.send(textMessage);   
+			
+			System.out.println("Lab-4:");
+			
+			QueueReceiver queueReceiver = queueSession.createReceiver(queue);
+			Message message = queueReceiver.receive();
+//			message.toString();
+			
+			System.out.println("Lab-5:" + message.toString());
+
+			System.out.println("Lab-6:");
+			
+	         
+		} catch (JMSException e) {
+			System.out.println("JMSException:" + e + "," +  e.getMessage());
+		} catch (NumberFormatException e) {
+			System.out.println("NumberFormatException:" + e);
+		} catch (Exception e) {
+			System.out.println("Exception:" + e);
+		} finally {
+			System.out.println("Lab-finally:");
+		}
+    	
+    }
 }
