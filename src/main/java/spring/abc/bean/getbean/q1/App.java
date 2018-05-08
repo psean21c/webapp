e58af9b3f37c1617bfe843a57c45e8e4c462047d
sungmin.park <psean21c@gmail.com>
2018-05-08 13:07:51 -0400

    7/8

--- src/main/java/spring/abc/bean/getbean/q1/App.java
+++ src/main/java/spring/abc/bean/getbean/q1/App.java
@@ -13,13 +13,37 @@ B) AirService service2 = context.getBean("airService1", AirService.class).
 C) AirService service3 = context.getBean(AirService).
 D) AirService service3 = context.getBean(AirService.class).
 
+>>
+spring-beans
+BeanFactory.getBean()
+
+<T> T	getBean(java.lang.Class<T> requiredType)
+<T> T	getBean(java.lang.Class<T> requiredType, java.lang.Object... args)
+Object	getBean(java.lang.String name)
+<T> T	getBean(java.lang.String name, java.lang.Class<T> requiredType)
+Object	getBean(java.lang.String name, java.lang.Object... args)
+
+https://www.javadoc.io/doc/org.springframework/spring-beans/5.0.5.RELEASE
+
+
  */
 public class App {
 	public static void main(String[] args) {
 		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("./spring/abc/bean/getbean/q1/beanQ1.xml");
+		
+		// 1)
 		AirService airService = (AirService) ctx.getBean("airService1");
 		System.out.println( "airService=" + airService.getName());
+		// 2)
+//		AirService airService2 = ctx.getBean("airService1", AirService.class);
+//		System.out.println( "airService2=" + airService2.getName());
+//		// 3)
+//		AirService airService3 = ctx.getBean(AirService);
+//		System.out.println( "airService3=" + airService3.getName());
+//		// 4)
+		AirService airService4 = ctx.getBean(AirService.class);
+		System.out.println( "airService4=" + airService4.getName());
 		
 		((ConfigurableApplicationContext)ctx).close();
 		
