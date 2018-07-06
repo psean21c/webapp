dcb5c794c61549a78981fbf4ce17607f19a42de5
sungmin.park <psean21c@gmail.com>
2018-07-06 09:37:19 -0400

    1

--- src/main/java/spring/autowire/MovieRecommender.java
+++ src/main/java/spring/autowire/MovieRecommender.java
@@ -9,8 +9,8 @@ public class MovieRecommender {
 	@Autowired
 	private MovieCatalog[] movieCatalogs;
 	
-//	private Map<String, MovieCatalog> movieCatalogs;
-//
+	private Map<String, MovieCatalog> movieCatalogs2;
+
 //
 //	@Autowired
 //	public void setMovieCatalogs(Map<String, MovieCatalog> movieCatalogs) {
@@ -18,14 +18,21 @@ public class MovieRecommender {
 //	}
 
 
+	/***
+	 * Constructor : 
+	 */
 	public MovieRecommender(){
 		movieCatalogs = new MovieCatalog[2];
+		movieCatalogs[0] = new MovieCatalog(0,"Matrix");
+		movieCatalogs[1] = new MovieCatalog(1,"GodFather");
+				
 		System.out.println("MovieRecommender created");
 	}
 	
 	public void write(){
 		for(MovieCatalog catalog: movieCatalogs){
-			System.out.println(catalog.toString());
+			//System.out.println(catalog.toString());
+			System.out.println(catalog.getTitle());
 		}
 	}
 }
