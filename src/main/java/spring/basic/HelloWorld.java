c3b2dfa90f6bd84ee0ae953f1c1f1abdebfdaf7b
sungmin.park <psean21c@gmail.com>
2018-08-02 09:27:49 -0400

    1

--- src/main/java/spring/basic/HelloWorld.java
+++ src/main/java/spring/basic/HelloWorld.java
@@ -4,8 +4,18 @@ public class HelloWorld {
 	private String message;
 
 	public void setMessage(String message) {
+		
 		this.message = message;
 	}
+	/***
+	public void getMessage() {
+	 * 
+	 * Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	The method println(boolean) in the type PrintStream is not applicable for the arguments (void)
+	at aop.com.AopMain.main(AopMain.java:13)
+	
+	Need to change void > String
+	 */
 
 	public void getMessage() {
 		System.out.println("Your Message : " + message);
